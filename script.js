d422dfca6d1bc789556f2eedae4115ee4fae0e55
Onur Kaya <onurkaya8@outlook.com>
2019-07-13 00:30:23 +0300

    navbar and footer added. script.js added for button position centering.

---
+++ script.js
@@ -0,0 +1,5 @@
+var $w = window.innerWidth;
+var elWidth = 335;
+var leftPos = ($w-elWidth)/2;
+document.getElementById('click-search').style.left=leftPos-7.5+'px';
+document.getElementById('click-lucky').style.left=leftPos+7.5+160+'px';
